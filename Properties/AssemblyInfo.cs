7f71f4277158f007962e143d50346e642b8cd8da
silverrad <conrad.haynes@hotmail.com>
2016-09-05 18:56:13 -0700

    init

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SilentTsuanami")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SilentTsuanami")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d77ec386-d2a0-4209-86d1-f27497da9c0f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ navigation.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $('#navigation').hide();
+});
+
+
+//display navigation
+$('#main').off().on("click", function () {
+    $('#navigation').toggle();
+});
+
+// Toggle header on selection
+$('#header').off().on("click", function () {
+    $('#header').toggle();
+});
+
+
+$('html').click(function (e) {
+    // Toggle header on controlled Click
+    if (e.pageY < 20) {
+        $('#header').toggle();
+     //   $('#header').addClass("slideDown");
+    }
+});
